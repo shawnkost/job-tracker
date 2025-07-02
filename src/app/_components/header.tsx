0222e7f19e43e8755ea57e0bca6dd09359bbb668
Shawn Kost <shawnmkost@gmail.com>
2025-07-02 16:39:04 -0700

    add avatar to header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
+import { auth } from "~/server/auth";
+import Image from "next/image";
 
-export function Header() {
+export async function Header() {
+  const session = await auth();
+
   return (
     <header className="bg-surface text-primary mb-4 flex items-center justify-between p-8">
       <div className="flex items-center gap-4">
@@ -12,6 +16,18 @@ export function Header() {
           <Link href="/board">Board</Link>
         </nav>
       </div>
+
+      {/* TODO: Add sign-out dropdown */}
+      {session?.user.image && session.user.name && (
+        <Image
+          className="border-border h-10 w-10 cursor-pointer rounded-full border object-cover"
+          src={session.user.image}
+          alt={session.user.name}
+          width={100}
+          height={100}
+          priority={true}
+        />
+      )}
     </header>
   );
 }
